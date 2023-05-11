dcd2a2f2c8938228dc605a36d562007541788502
maybb2003 <maybb2003@gmail.com>
2023-05-11 14:39:47 +0300

    feat: added interfaces

---
+++ src/recipes/recipes.interface.ts
@@ -0,0 +1,31 @@
+export interface IRecipe { 
+    id?: string;
+    recipeId: number;
+    mealType: MealType;
+    macros: IMacros;
+    price: number;
+    purchaseDate: Date;
+}
+  
+export interface IMacros { 
+    calories: number;
+    protein: number;
+    carbs: number;
+    fat: number;
+    cholesterol: number;
+    fiber: number;
+    sodium: number;
+    water: number;
+    calcium: number;
+    iron: number;
+    magnesium: number;
+    phosphorus: number;
+    zinc: number;
+    potassium: number;
+}
+
+export enum MealType {
+    breakfast = "BREAKFAST",
+    lunch = "LUNCH",
+    dinner = "DINNER"
+}
