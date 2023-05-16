1a24a492b2e7537525c320b89487170cb1c9f035
maybb2003 <maybb2003@gmail.com>
2023-05-16 12:54:35 +0300

    feat: added router

--- src/users/users.router.ts
+++ src/users/users.router.ts
@@ -8,4 +8,50 @@ import { UsersController } from "./users.controller";
 
 const UsersRouter: Router = Router();
 
+UsersRouter.post(
+  "/createUser",
+  ValidateRequest(createUserReqSchema),
+  wrapAsync(UsersController.createUser)
+);
+UsersRouter.get(
+  "/createUser",
+  ValidateRequest(getAllUsersReqSchema),
+  wrapAsync(UsersController.getAllUsers)
+);
+UsersRouter.get(
+  "/createUser",
+  ValidateRequest(getUserByIdReqSchema),
+  wrapAsync(UsersController.getUserById)
+);
+UsersRouter.get(
+  "/createUser",
+  ValidateRequest(getUserByCountryReqSchema),
+  wrapAsync(UsersController.getUserByCountry)
+);
+UsersRouter.get(
+  "/createUser",
+  ValidateRequest(getUserByAgeReqSchema),
+  wrapAsync(UsersController.getUserByAge)
+);
+UsersRouter.get(
+  "/createUser",
+  ValidateRequest(getUserByGenderReqSchema),
+  wrapAsync(UsersController.getUserByGender)
+);
+UsersRouter.delete(
+  "/createUser",
+  ValidateRequest(deleteUserByIdReqSchema),
+  wrapAsync(UsersController.deleteUserById)
+);
+UsersRouter.put(
+  "/createUser",
+  ValidateRequest(updateUserDetailsByIdReqSchema),
+  wrapAsync(UsersController.updateUserDetailsById)
+);
+UsersRouter.put(
+  "/createUser",
+  ValidateRequest(updateUserFitnessProfileByIdReqSchema),
+  wrapAsync(UsersController.updateUserFitnessProfileById)
+);
+
 export { UsersRouter };
