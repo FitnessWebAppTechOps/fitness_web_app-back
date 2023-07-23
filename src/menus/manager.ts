2705a26dd8d09744da3e3c2b57e501ce52e5492b
cr9169 <bar.gabay.home@gmail.com>
2023-07-23 10:37:54 +0300

    hi

--- src/menus/manager.ts
+++ src/menus/manager.ts
@@ -3,27 +3,38 @@ import { IMenu, IMenuDocument } from "./interface";
 import { MenusModel } from "./model";
 
 export class MenusManager {
-    static async createMenu(menu: IMenu): Promise<IMenuDocument> {
-        return MenusModel.create(menu);
-    }
+  static async createMenu(menu: IMenu): Promise<IMenuDocument> {
+    return MenusModel.create(menu);
+  }
 
-    static async getAllMenus(): Promise<IMenuDocument[]> {
-        return MenusModel.find();
-    }
+  static async getAllMenus(): Promise<IMenuDocument[]> {
+    return MenusModel.find();
+  }
 
-    static async getMenuById(menuId: string): Promise<IMenuDocument> {
-        return MenusModel.findById(menuId).orFail(new DocumentNotFoundError(menuId)).exec();
-    } 
+  static async getMenuById(menuId: string): Promise<IMenuDocument> {
+    return MenusModel.findById(menuId)
+      .orFail(new DocumentNotFoundError(menuId))
+      .exec();
+  }
 
-    static async getMenuByUserId(userId: string): Promise<IMenuDocument> {
-        return MenusModel.findById(userId).orFail(new DocumentNotFoundError(userId)).exec();
-    }
+  static async getMenuByUserId(userId: string): Promise<IMenuDocument> {
+    return MenusModel.findById(userId)
+      .orFail(new DocumentNotFoundError(userId))
+      .exec();
+  }
 
-    static async updateMenu(menuId: string, update: Partial<IMenu>): Promise<IMenuDocument> {
-        return MenusModel.findByIdAndUpdate(menuId, update, { new: true }).orFail(new DocumentNotFoundError(menuId)).exec();
-    } 
+  static async updateMenuById(
+    menuId: string,
+    update: Partial<IMenu>
+  ): Promise<IMenuDocument> {
+    return MenusModel.findByIdAndUpdate(menuId, update, { new: true })
+      .orFail(new DocumentNotFoundError(menuId))
+      .exec();
+  }
 
-    static async deleteMenu(menuId: string): Promise<IMenuDocument> {
-        return MenusModel.findByIdAndDelete(menuId).orFail(new DocumentNotFoundError(menuId)).exec();
-    }
+  static async deleteMenu(menuId: string): Promise<IMenuDocument> {
+    return MenusModel.findByIdAndDelete(menuId)
+      .orFail(new DocumentNotFoundError(menuId))
+      .exec();
+  }
 }
